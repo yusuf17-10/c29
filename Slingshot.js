0cf13b5e2006121601f6e4f41e747da885855272
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2020-05-28 21:56:01 +0530

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -34,13 +34,13 @@ class SlingShot{
                 strokeWeight(7);
                 line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
                 line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
-                image(this.sling3.pointA.x-30,pointA.y-10,15,30);
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
                 
             }else{
                 strokeWeight(3);
             line(pointA.x-25, pointA.y, pointB.x-10, pointB.y);
             line(pointA.x-25, pointA.y, pointB.x+30, pointB.y-3);
-            image(this.sling3.pointA.x-25,pointA.y-10,15,30);
+            image(this.sling3,pointA.x-25,pointA.y-10,15,30);
             }
             pop();
 
